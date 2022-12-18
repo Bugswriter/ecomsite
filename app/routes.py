2924d5da3c4f5031444c6f453b613e7fddb122e6
Suraj Kushwah <suraj@bugswriter.com>
2022-12-19 00:57:53 +0530

    adding skelecton

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,16 @@ from flask import render_template
 @app.route('/')
 def home():
 	return render_template("home.html")
+
+@app.route('/shop')
+def products():
+	return render_template("shop.html")
+
+@app.route('/profile')
+def profile():
+	return render_template("profile.html")
+
+@app.route('/payout')
+def payout():
+	return render_template("payout.html")
+
